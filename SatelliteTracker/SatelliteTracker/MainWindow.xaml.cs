053264e54fa361def2288bf68a21c22b3ff3a99c
ART1 <mahlaevartur@gmail.com>
2026-05-01 01:47:22 +0300

    Инит коммит, +быстрый UI, и отрисовка OpenStreetMap

---
+++ SatelliteTracker/SatelliteTracker/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Web.WebView2.Core;
+
+namespace SatelliteTracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitMap();
+            this.WindowState = WindowState.Maximized;
+        }
+
+        private async void InitMap()
+        {
+            await MapView.EnsureCoreWebView2Async();
+
+            string htmlPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "map.html");
+            MapView.CoreWebView2.Navigate(new Uri(htmlPath).AbsoluteUri);
+        }
+    }
+}
